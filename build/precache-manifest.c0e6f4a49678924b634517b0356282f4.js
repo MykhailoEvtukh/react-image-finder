ec6f21a72d297dc51cf9e5d748b255a2b0ca6c3d
MishaMiron <53948817+MykhailoEvtukh@users.noreply.github.com>
2020-01-23 12:25:30 +0200

    last edit and deploy for image-finder

---
+++ build/precache-manifest.c0e6f4a49678924b634517b0356282f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8089fe15a1cf8e0369880161001d9e",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "77d1522d16540c2967c3",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.8e6f3ec2.chunk.css"
+  },
+  {
+    "revision": "7994a29655acb7aaa382",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a2f81a65.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a2f81a65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77d1522d16540c2967c3",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.35859929.chunk.js"
+  },
+  {
+    "revision": "53e06b259336ac6825a8",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.b1e89add.js"
+  }
+]);
