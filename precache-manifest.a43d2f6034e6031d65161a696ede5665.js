846dfb9c227368cf20969d5d14c014d61f515bb9
MishaMiron <53948817+MykhailoEvtukh@users.noreply.github.com>
2020-03-16 12:09:33 +0200

    Updates

---
+++ precache-manifest.a43d2f6034e6031d65161a696ede5665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e276e995797ac6658bc94e272de374",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "9a25d17b4d0bbb73b3d7",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.3973fa12.chunk.css"
+  },
+  {
+    "revision": "ab563f46def243cd742b",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.adf3f3a2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.adf3f3a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a25d17b4d0bbb73b3d7",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.bf13f9e4.chunk.js"
+  },
+  {
+    "revision": "2f418340f03c9210876f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.10d7b645.js"
+  }
+]);
